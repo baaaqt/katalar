754f5aa5db3741c07efb3d0e3b58e70d5f69f7eb
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2024-11-08 22:43:16 +0700

    add UpdatedAtMixin for automatic update timestamp management

--- core/core/database/entities/mixins/datetimesmixin.py
+++ core/core/database/entities/mixins/datetimesmixin.py
@@ -5,3 +5,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class CreatedAtMixin:
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+
+
+class UpdatedAtMixin:
+    updated_at: Mapped[datetime] = mapped_column(
+        default=datetime.now, onupdate=datetime.now
+    )
