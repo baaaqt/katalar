e2749c4397cee656e250dc5746c1bade9cf21f0a
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2024-11-08 22:43:02 +0700

    remove updated_at field from CreatedAtMixin for simplified timestamp management

--- core/core/database/entities/mixins/datetimesmixin.py
+++ core/core/database/entities/mixins/datetimesmixin.py
@@ -5,7 +5,3 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class CreatedAtMixin:
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(
-        default=datetime.now,
-        onupdate=datetime.now,
-    )
