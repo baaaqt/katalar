6fe431173ac952152f441aace5032854b0b211a1
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2024-11-08 23:16:09 +0700

    refactor Tag entity to inherit from CreatedAtMixin and set name as primary key

--- core/core/database/entities/tag.py
+++ core/core/database/entities/tag.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.database.entities.base import Entity
+from core.database.entities.mixins.datetimesmixin import CreatedAtMixin
 
 
-class Tag(Entity):
+class Tag(Entity, CreatedAtMixin):
     __tablename__ = "tags"
 
-    name: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str] = mapped_column(unique=True, primary_key=True)
