c5bf92765fb7048dacadc73a23a9335206f24d8f
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2024-11-10 21:11:01 +0700

    refactor repositories to include entity-specific types for User, Project, Record, Tag, TagToken, and UserTag

--- core/core/database/repos.py
+++ core/core/database/repos.py
@@ -1,17 +1,40 @@
+from core.database.entities.project import Project
+from core.database.entities.record import Record
+from core.database.entities.tag import Tag
+from core.database.entities.tagtoken import TagToken
+from core.database.entities.user import User
+from core.database.entities.user_tag import UserTag
 from core.database.patterns.repositories.id_repository import IDEntityRepository
 from core.database.patterns.repositories.pagination import PaginationRepository
 
 
-class UserRepositoryBase(IDEntityRepository, PaginationRepository): ...
+class UserRepositoryBase(IDEntityRepository[User, str], PaginationRepository[User]):
+    entity = User
 
 
-class ProjectRepositoryBase(IDEntityRepository, PaginationRepository): ...
+class ProjectRepositoryBase(
+    IDEntityRepository[Project, str],
+    PaginationRepository[Project],
+):
+    entity = Project
 
 
-class RecordRepositoryBase(IDEntityRepository, PaginationRepository): ...
+class RecordRepositoryBase(
+    IDEntityRepository[Record, str], PaginationRepository[Record]
+):
+    entity = Record
 
 
-class TagRepositoryBase(PaginationRepository): ...
+class TagRepositoryBase(PaginationRepository[Tag]):
+    entity = Tag
 
 
-class TagTokenRepositoryBase(IDEntityRepository, PaginationRepository): ...
+class TagTokenRepositoryBase(
+    IDEntityRepository[TagToken, str],
+    PaginationRepository[TagToken],
+):
+    entity = TagToken
+
+
+class UserTagRepositoryBase(PaginationRepository[UserTag]):
+    entity = UserTag
