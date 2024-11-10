ce03b81cc6f0b4c3a1e7ffc96e60cef3965fef2c
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2024-11-10 12:25:50 +0700

    add ProjectTag entity with unique constraint for project-tag relationships

---
+++ core/core/database/entities/project_tag.py
@@ -0,0 +1,16 @@
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.core.database.entities.base import Entity
+
+
+class ProjectTag(Entity):
+    __tablename__ = "projects_tags"
+    project_id: Mapped[str] = mapped_column(ForeignKey("projects.id"), primary_key=True)
+    tag_name: Mapped[str] = mapped_column(ForeignKey("tags.name"), primary_key=True)
+
+    __table_args__ = (
+        UniqueConstraint(
+            "project_id", "tag_name", name="project_tag_unique_constraint"
+        ),
+    )
