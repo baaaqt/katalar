60816ff82805ab249f307a9c206b8f001eb28559
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2024-11-10 12:25:56 +0700

    add relationship for tags in Project entity to support many-to-many associations

--- core/core/database/entities/project.py
+++ core/core/database/entities/project.py
@@ -1,6 +1,7 @@
 from sqlalchemy import String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from core.core.database.entities.tag import Tag
 from core.database.entities.base import Entity
 from core.database.entities.mixins.id import IDAsUUIDStringMixin
 
@@ -11,3 +12,8 @@ class Project(IDAsUUIDStringMixin, Entity):
     name: Mapped[str] = mapped_column(String(length=255), unique=True)
     description: Mapped[str] = mapped_column(Text())
     is_active: Mapped[bool] = mapped_column(default=True, index=True)
+    tags: Mapped[list[Tag]] = relationship(
+        "Tag",
+        secondary="projects_tags",
+        lazy="joined",
+    )
