4e8df324eb3b225a45dc31fbab6c95139ff2d8d4
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2024-11-10 12:31:22 +0700

    add TagToken entity to establish relationship with tags and define token field

---
+++ core/core/database/entities/tagkey.py
@@ -0,0 +1,10 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.core.database.entities.base import Entity
+from core.core.database.entities.mixins.id import IDAsUUIDStringMixin
+
+
+class TagToken(Entity, IDAsUUIDStringMixin):
+    tag_name: Mapped[str] = mapped_column(ForeignKey("tags.name"), primary_key=True)
+    token: Mapped[str]
